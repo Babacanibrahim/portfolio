2dad796223ac486aa3ce931f5ecf1e71f4a87bda
Babacanİbrahim <babacan-1907@outlook.com.tr>
2025-07-30 22:33:37 +0300

    Birden fazla fotograf ekleyebilme ozellikleri tanimlanir.

--- config.py
+++ config.py
@@ -3,9 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
-    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "static/uploads")
+    
+    RELATIVE_UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "static/uploads")
+    UPLOAD_FOLDER = os.path.join(basedir, 'app', RELATIVE_UPLOAD_FOLDER)
+    
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///portfolio.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
